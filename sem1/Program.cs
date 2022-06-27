3052e7549a1ca0450075b681d3f2cfb66cdba5c9
Kannika Aleksandr <car113man@gmail.com>
2022-06-28 00:54:29 +0300

    sem1 от-N до N

--- sem1/Program.cs
+++ sem1/Program.cs
@@ -12,7 +12,7 @@ Console.Write("Квадрат числа равен: " + squareNumber);
 // a = 9, b = -3 -> да 
 // a = -3 b = 9 -> нет
 
-Console.Write("введите 1 число: ");
+/*Console.Write("введите 1 число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("введите 2 число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
@@ -27,10 +27,10 @@ else
     Console.Write(number2);
     Console.WriteLine(" не является квадратом " + number1);
 }
-
+*/
 
 //3.Напишите программу, которая будет выдавать название дня недели по заданному номеру.
-
+/*
 Console.Write("Введите номер дня недели: ");
 int dayNumber = Convert.ToInt32(Console.ReadLine());
 if (1<= dayNumber && dayNumber <= 7)
@@ -74,4 +74,15 @@ if (1<= dayNumber && dayNumber <= 7)
 else
 {
     Console.Write("нужно ввести от 1 до 7");
+}
+*/
+//5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+Console.Write("N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int negN = N * (-1);
+while (negN <= N)
+{
+    Console.WriteLine(negN);
+    negN++;
 }
