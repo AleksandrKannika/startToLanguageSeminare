b342a77b1cdbed046074590d0b13dc2145e05a66
Kannika Aleksandr <car113man@gmail.com>
2022-07-22 06:38:49 +0300

    9 home Work

--- sem9/Program.cs
+++ sem9/Program.cs
@@ -51,29 +51,29 @@
 // M = 1; N = 5. -> "1, 2, 3, 4, 5"
 // M = 4; N = 8. -> "4, 6, 7, 8"
 
-// int minNumber = Convert.ToInt32(Console.ReadLine());
-// int maxNumber = Convert.ToInt32(Console.ReadLine());
+int minNumber = Convert.ToInt32(Console.ReadLine());
+int maxNumber = Convert.ToInt32(Console.ReadLine());
 
-// string NaturalNumbers(int m, int n)
-// {
-//     if(m==n) return n.ToString();
-//     return ($"{m}, {NaturalNumbers(m+1,n)}");
-// }
-// Console.WriteLine(NaturalNumbers(minNumber,maxNumber));
+string NaturalNumbers(int m, int n)
+{
+    if(m==n) return n.ToString();
+    return ($"{m}, {NaturalNumbers(m+1,n)}");
+}
+Console.WriteLine(NaturalNumbers(minNumber,maxNumber));
 
 // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-// int minElement = Convert.ToInt32(Console.ReadLine());
-// int maxElement = Convert.ToInt32(Console.ReadLine());
+int minElement = Convert.ToInt32(Console.ReadLine());
+int maxElement = Convert.ToInt32(Console.ReadLine());
 
-// int NaturalNumbers(int n, int m)
-// {
-//     if(m==n) return m;
-//     return (m+NaturalNumbers(m+1,n));
-// }
-// Console.WriteLine(NaturalNumbers(minElement,maxElement));
+int NaturalNumbers(int n, int m)
+{
+    if(m==n) return m;
+    return (m+NaturalNumbers(m+1,n));
+}
+Console.WriteLine(NaturalNumbers(minElement,maxElement));
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n. 
 // m = 2, n = 3 -> A(m,n) = 29
