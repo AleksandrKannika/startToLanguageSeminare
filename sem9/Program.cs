b0e36398498ccacb796c3abb56424b3183ced293
Kannika Aleksandr <car113man@gmail.com>
2022-07-22 06:51:58 +0300

    9 home work

--- sem9/Program.cs
+++ sem9/Program.cs
@@ -51,15 +51,15 @@
 // M = 1; N = 5. -> "1, 2, 3, 4, 5"
 // M = 4; N = 8. -> "4, 6, 7, 8"
 
-int minNumber = Convert.ToInt32(Console.ReadLine());
-int maxNumber = Convert.ToInt32(Console.ReadLine());
+// int minNumber = Convert.ToInt32(Console.ReadLine());
+// int maxNumber = Convert.ToInt32(Console.ReadLine());
 
-string NaturalNumbers(int m, int n)
-{
-    if(m==n) return n.ToString();
-    return ($"{m}, {NaturalNumbers(m+1,n)}");
-}
-Console.WriteLine(NaturalNumbers(minNumber,maxNumber));
+// string NaturalNumbers(int m, int n)
+// {
+//     if(m==n) return n.ToString();
+//     return ($"{m}, {NaturalNumbers(m+1,n)}");
+// }
+// Console.WriteLine(NaturalNumbers(minNumber,maxNumber));
 
 // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
@@ -68,22 +68,22 @@ Console.WriteLine(NaturalNumbers(minNumber,maxNumber));
 int minElement = Convert.ToInt32(Console.ReadLine());
 int maxElement = Convert.ToInt32(Console.ReadLine());
 
-int NaturalNumbers(int n, int m)
+int SummNatural(int m, int n)
 {
     if(m==n) return m;
-    return (m+NaturalNumbers(m+1,n));
+    return (m+SummNatural(m+1,n));
 }
-Console.WriteLine(NaturalNumbers(minElement,maxElement));
+Console.WriteLine(SummNatural(minElement,maxElement));
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n. 
 // m = 2, n = 3 -> A(m,n) = 29
 
-int Akkerman(int m, int n)
-{
-    if (m == 0) return n + 1;
-    if (m > 0 && n == 0) return Akkerman(m - 1, 1);
-    if (m > 0 && n > 0) return Akkerman(m - 1,Akkerman(m,n-1));
+// int Akkerman(int m, int n)
+// {
+//     if (m == 0) return n + 1;
+//     if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+//     if (m > 0 && n > 0) return Akkerman(m - 1,Akkerman(m,n-1));
 
-    return Akkerman(m,n); 
-}
-Console.WriteLine(Akkerman(3,5));
+//     return Akkerman(m,n); 
+// }
+// Console.WriteLine(Akkerman(3,5));
